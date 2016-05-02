386f4def95a6ba77319573c12a47a423e4797d73
asuki <asukiaaa@gmail.com>
2016-05-02 10:56:41 +0900

    Initial commit.

---
+++ SevenBotController.cpp
@@ -0,0 +1,111 @@
+#include "SevenBotController.h"
+
+SevenBotController::SevenBotController() {
+  base_hori_servo = new KPowerServoController(2, 0);
+
+  base_vert_servo = new KPowerServoController(3, 1);
+  base_vert_servo->max_axis = 190;
+
+  middle_vert_servo = new KPowerServoController(4, 2);
+  middle_vert_servo->min_axis = -24;
+  middle_vert_servo->max_axis = 40;
+
+  middle_roll_servo = new KPowerServoController(5, 3);
+  middle_roll_servo->max_axis = 170;
+
+  top_vert_servo = new KPowerServoController(6, 4);
+  top_vert_servo->min_axis = -30;
+
+  top_roll_servo    = new KPowerServoController(7, 5);
+  top_roll_servo->min_axis = -20;
+  top_roll_servo->max_axis = 190;
+
+  servos[0] = base_hori_servo;
+  servos[1] = base_vert_servo;
+  servos[2] = middle_vert_servo;
+  servos[3] = middle_roll_servo;
+  servos[4] = top_vert_servo;
+  servos[5] = top_roll_servo;
+
+  servo_length = 6;
+}
+
+void SevenBotController::power_on() {
+  //servos[0]->power_on();
+  //servos[1]->power_on();
+  //servos[2]->power_on();
+  for(i=0; i<servo_length; i++) {
+    servos[i]->power_on();
+  }
+}
+
+void SevenBotController::power_off() {
+  for (i=0; i<servo_length; i++) {
+    servos[i]->power_off();
+  }
+}
+
+String SevenBotController::get_axis_str() {
+  String axis_str;
+
+  for (i=0; i<servo_length; i++) {
+    axis_str += servos[i]->get_sensor_axis();
+
+    // put ',' except for last one
+    if (i != servo_length -1) {
+      axis_str += ",";
+    }
+  }
+
+  return axis_str;
+}
+
+boolean SevenBotController::update_target_axis(String axis_str) {
+  char str_separator = ',';
+
+  int begin_index;
+  int end_index = 0;
+  String str_value;
+  int axis_list[6];
+  int axis_index = 0;
+
+  for (axis_index=0; axis_index<6; axis_index++) {
+    if (end_index == -1) {
+      break;
+    }
+
+    if (axis_index == 0) {
+      begin_index = 0;
+    } else {
+      begin_index = end_index + 1;
+    }
+
+    end_index = axis_str.indexOf(str_separator, begin_index);
+    //Serial.print("end index is: ");
+    //Serial.println(end_index);
+    if (end_index == -1) {
+      str_value = axis_str.substring(begin_index);
+    } else {
+      str_value = axis_str.substring(begin_index, end_index);
+    }
+    axis_list[axis_index] = str_value.toInt();
+    //Serial.println(axis_list[i]);
+  }
+
+  // axis1 + axis2 have to be between 90 and 130
+  if (axis_index == 2) {
+    //axis[1]
+  } else if (axis_index >= 3) {
+  }
+
+  for (i=0; i<axis_index; i++) {
+    servos[i]->set_target_axis(axis_list[i]);
+  }
+}
+
+boolean SevenBotController::update_axis() {
+  for (i=0; i<servo_length; i++) {
+    servos[i]->update_axis();
+  }
+}
+
---
+++ SevenBotController.h
@@ -0,0 +1,29 @@
+#ifndef SevenBotController_h
+#define SevenBotController_h
+#include <Arduino.h>
+#include <KPowerServoController.h>
+
+class SevenBotController {
+  private:
+  KPowerServoController* servos[6];
+  int servo_length;
+  int i;
+
+  public:
+  SevenBotController();
+
+  KPowerServoController* base_hori_servo;
+  KPowerServoController* base_vert_servo;
+  KPowerServoController* middle_vert_servo;
+  KPowerServoController* middle_roll_servo;
+  KPowerServoController* top_vert_servo;
+  KPowerServoController* top_roll_servo;
+
+  void power_on();
+  void power_off();
+  String get_axis_str();
+  boolean update_target_axis(String str_axis_info);
+  boolean update_axis();
+};
+
+#endif
