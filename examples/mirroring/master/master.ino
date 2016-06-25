b450729348317186f676eea9d0924fb168ea6a60
asukiaaa <asukiaaa@gmail.com>
2016-06-25 10:09:09 +0900

    Added examples about mirroring.

---
+++ examples/mirroring/master/master.ino
@@ -0,0 +1,14 @@
+#include "SevenBotController.h"
+
+SevenBotController seven_bot;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(115200);
+}
+
+void loop() {
+  seven_bot.send_serial_as_master(&Serial1);
+  //seven_bot.send_serial_as_master(&Serial);
+}
+
---
+++ examples/mirroring/slave/slave.ino
@@ -0,0 +1,18 @@
+#include "SevenBotController.h"
+
+SevenBotController seven_bot;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(115200);
+  delay(1000);
+  seven_bot.power_on();
+  //seven_bot.power_off(2);
+}
+
+void loop() {
+  seven_bot.receive_serial_as_slave(&Serial1);
+  //seven_bot.receive_serial_as_slave(&Serial);
+  seven_bot.update_axis();
+}
+
